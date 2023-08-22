127f63c96670084fa3262c1c113bcb98202225b8
Bodhisatwa Chakraborty <bodhisatwachakraborty2002@gmail.com>
2023-08-23 00:46:24 +0530

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+
+import Wrapper from "./Wrapper";
+import gmailIcon from "../assets/email-icon.png";
+import Github from "../assets/github.png";
+import Instagram from "../assets/instagram.png";
+import LinkedIn from "../assets/linkedin.png";
+import { scrollTo } from "../helper";
+
+const Footer = () => {
+  return (
+    <div className="bg-[#F2F2F2]">
+      <Wrapper>
+        {/* NAVBAR START */}
+        <div className="flex flex-col md:flex-row items-center justify-between gap-5 2xl:mt-[63px] relative text-[#111111] border-t border-[#CCCCCC] py-10">
+          <div className="flex items-center gap-[6px]">
+            <div className="w-[35px] h-[35px] rounded-full bg-[#252525] flex justify-center items-center">
+              <img src={gmailIcon} alt="" className="w-[18px]" />
+            </div>
+            <div>bodhisatwachakraborty2002@gmail.com</div>
+          </div>
+          <div
+            className="social-links"
+            style={{
+              display: "flex",
+              gap: "3rem",
+            }}
+          >
+            <a href="https://github.com/Bodhisatwa2002/">
+              <img
+                style={{
+                  width: "2rem",
+                  height: "2rem",
+                  cursor: "pointer",
+                }}
+                src={Github}
+                alt=""
+              />
+            </a>
+            <a href="https://www.instagram.com/_morphineee____/">
+              <img
+                style={{ width: "2rem", height: "2rem", cursor: "pointer" }}
+                src={Instagram}
+                alt=""
+              />
+            </a>
+            <a href="https://www.linkedin.com/in/bodhisatwa-chakraborty-freelancer/">
+              <img
+                style={{ width: "2rem", height: "2rem", cursor: "pointer" }}
+                src={LinkedIn}
+                alt=""
+              />
+            </a>
+          </div>
+          <ul className="flex 2xl:text-[20px] gap-5 md:gap-8">
+            <li
+              className="cursor-pointer transition active:scale-90"
+              onClick={() => scrollTo("about")}
+            >
+              About me
+            </li>
+            <li
+              className="cursor-pointer transition active:scale-90"
+              onClick={() => scrollTo("skills")}
+            >
+              Skills
+            </li>
+            <li
+              className="cursor-pointer transition active:scale-90"
+              onClick={() => scrollTo("work")}
+            >
+              Projects
+            </li>
+            <li
+              className="cursor-pointer transition active:scale-90"
+              onClick={() => scrollTo("contact")}
+            >
+              Contact
+            </li>
+          </ul>
+        </div>
+        {/* NAVBAR END */}
+      </Wrapper>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/data.js
@@ -0,0 +1,77 @@
+import p1 from "./assets/project-1.png";
+import p2 from "./assets/project-2.png";
+import p3 from "./assets/project-3.png";
+import p4 from "./assets/project-4.png";
+import p5 from "./assets/project-5.png";
+import p6 from "./assets/project-6.png";
+
+export const filters = [
+    {
+        name: "All",
+        id: "all",
+    },
+    {
+        name: "Websites",
+        id: "website",
+    },
+    {
+        name: "Designs",
+        id: "design",
+    },
+];
+
+export const projects = [
+  {
+    name: "Carpediem 3.0",
+    image: p1,
+    tags: ["website"],
+  },
+  {
+    name: "FitClub",
+    image: p2,
+    tags: ["website"],
+  },
+  {
+    name: "Social Media",
+    image: p3,
+    tags: ["website"],
+  },
+  {
+    name: "WeCare",
+    image: p4,
+    tags: ["website"],
+  },
+  {
+    name: "Drum Kit",
+    image: p5,
+    tags: ["website"],
+  },
+  {
+    name: "12th Man",
+    image: p6,
+    tags: ["design"],
+  },
+];
+
+export const achievements = [
+    {
+        id: 1,
+        year: 80,
+        description: "SUCCESSFULLY<br />COMPLETED<br />PROJECTS",
+    },
+    {
+        id: 2,
+        year: 15,
+        description: "YEARS OF<br />EXPERIENCE",
+    },
+    {
+        id: 3,
+        year: 25,
+        description: "TECHNOLOGIES",
+    },
+    {
+        id: 4,
+        year: 70,
+        description: "COMMUNITY<br />POSTS",
+    },
+];
